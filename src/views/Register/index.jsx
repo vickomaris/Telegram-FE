6cf086c88c021596107d8f61ef9b7e9ff62ebd49
vickomaris <64400928+vickomaris@users.noreply.github.com>
2022-12-13 05:36:30 +0700

    fix register deploy

--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -18,32 +18,28 @@ const Index = () => {
     e.preventDefault();
     // console.log(form)
     if (form.username == "" || form.email == "" || form.password == "") {
-        alert('Semua input wajib diisi')
+      alert('Semua input wajib diisi')
     } else {
-        const body = {
-            username: form.username,
-            email: form.email,
-            password: form.password,
-        }
-        
-            axios.post(`${process.env.REACT_APP_BACKEND_URL}/register`, body)
-                .then((response) => {
-                    if (response.data.status != 'success') {
-                        alert(response.data.message)
-                    } else {
-                        alert("data berhasil ditambahkan")
-                        console.log(response.data)
-                        return navigate('/')
-                    }
-                    // console.log(response.data)
-                    // return navigate('/')
-                }).catch((err) => {
-                    console.error(err)
-                })
-        
+      const body = {
+        username: form.username,
+        email: form.email,
+        password: form.password,
+      }
+
+      axios.post(`${process.env.REACT_APP_BACKEND_URL}/register`, body)
+        .then((response) => {
+          alert("data berhasil ditambahkan")
+          console.log(response.data)
+          return navigate('/')
+          // console.log(response.data)
+          // return navigate('/')
+        }).catch((err) => {
+          console.error(err)
+        })
+
     }
-}
-  
+  }
+
   return (
     <>
       <section className={`d-flex align-items-center ${styles.main}`}>
@@ -51,40 +47,40 @@ const Index = () => {
           <div className="row">
             <div className={`col-md-5 p-5 m-auto ${styles.sideLogin}`}>
               <div className="d-flex flex-row">
-                <img src={icBack} alt="logo"  />
+                <img src={icBack} alt="logo" />
                 <div className={`mx-auto ${styles.title}`}>Register</div>
               </div>
               <p className={`mt-5 ${styles.subTitle}`}>Let’s create your account!</p>
               <form onSubmit={(e) => onSubmit(e)}>
                 <div className="form-floating mb-3">
-                <input type="text" 
-                className={`form-control ${styles.inputForm}`} 
-                id="floatingInput" 
-                placeholder="Telegram app"
-                onChange={(e) => setForm({ ...form, username: e.target.value })} />
-                <label htmlFor="floatingInput" className="form-label">Name</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input type="email" 
-                className={`form-control ${styles.inputForm}`} 
-                id="floatingInput" 
-                placeholder="name@example.com"
-                onChange={(e) => setForm({ ...form, email: e.target.value })} />
-                <label htmlFor="floatingInput" className="form-label">Email address</label>
-              </div>
-              <div className="form-floating">
-                <input type="password" 
-                className={`form-control ${styles.inputForm}`} 
-                id="floatingPassword" 
-                placeholder="Password"
-                onChange={(e) => setForm({ ...form, password: e.target.value })} />
-                <label htmlFor="floatingPassword" className="form-label">Password</label>
-              </div>
-              <div className="d-flex flex-column mt-4">
-                <button type="submit" className={styles.btnRegister}>Register</button>
-                <p className="my-4 text-center textLW">Register With</p>
-                <button className={styles.btnGLogin}><img src={icGoogle} alt="icGoogle" className="me-3"/> Google </button>
-              </div>
+                  <input type="text"
+                    className={`form-control ${styles.inputForm}`}
+                    id="floatingInput"
+                    placeholder="Telegram app"
+                    onChange={(e) => setForm({ ...form, username: e.target.value })} />
+                  <label htmlFor="floatingInput" className="form-label">Name</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input type="email"
+                    className={`form-control ${styles.inputForm}`}
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                    onChange={(e) => setForm({ ...form, email: e.target.value })} />
+                  <label htmlFor="floatingInput" className="form-label">Email address</label>
+                </div>
+                <div className="form-floating">
+                  <input type="password"
+                    className={`form-control ${styles.inputForm}`}
+                    id="floatingPassword"
+                    placeholder="Password"
+                    onChange={(e) => setForm({ ...form, password: e.target.value })} />
+                  <label htmlFor="floatingPassword" className="form-label">Password</label>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                  <button type="submit" className={styles.btnRegister}>Register</button>
+                  <p className="my-4 text-center textLW">Register With</p>
+                  <button className={styles.btnGLogin}><img src={icGoogle} alt="icGoogle" className="me-3" /> Google </button>
+                </div>
               </form>
             </div>
           </div>
